9124d2adde70853adb3c97e1e824afc5171c1d09
исп-31 <-31@LAB17-05>
2020-12-21 12:21:05 +0300

    Добавьте файлы проекта.

---
+++ 16/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace _16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBG = new System.Windows.Forms.PictureBox();
+            this.pictureBoxDish = new System.Windows.Forms.PictureBox();
+            this.pictureBoxFruit = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.стартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFruit)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBG
+            // 
+            this.pictureBG.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBG.BackgroundImage")));
+            this.pictureBG.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBG.Location = new System.Drawing.Point(-3, 21);
+            this.pictureBG.Name = "pictureBG";
+            this.pictureBG.Size = new System.Drawing.Size(1105, 906);
+            this.pictureBG.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBG.TabIndex = 0;
+            this.pictureBG.TabStop = false;
+            // 
+            // pictureBoxDish
+            // 
+            this.pictureBoxDish.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxDish.Image")));
+            this.pictureBoxDish.Location = new System.Drawing.Point(460, 840);
+            this.pictureBoxDish.Name = "pictureBoxDish";
+            this.pictureBoxDish.Size = new System.Drawing.Size(211, 87);
+            this.pictureBoxDish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxDish.TabIndex = 1;
+            this.pictureBoxDish.TabStop = false;
+            // 
+            // pictureBoxFruit
+            // 
+            this.pictureBoxFruit.Location = new System.Drawing.Point(519, 21);
+            this.pictureBoxFruit.Name = "pictureBoxFruit";
+            this.pictureBoxFruit.Size = new System.Drawing.Size(98, 76);
+            this.pictureBoxFruit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxFruit.TabIndex = 2;
+            this.pictureBoxFruit.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стартToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1101, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // стартToolStripMenuItem
+            // 
+            this.стартToolStripMenuItem.Name = "стартToolStripMenuItem";
+            this.стартToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.стартToolStripMenuItem.Text = "Старт";
+            this.стартToolStripMenuItem.Click += new System.EventHandler(this.стартToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 927);
+            this.Controls.Add(this.pictureBoxFruit);
+            this.Controls.Add(this.pictureBoxDish);
+            this.Controls.Add(this.pictureBG);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Поймай фрукт тарелкой!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFruit)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBG;
+        private System.Windows.Forms.PictureBox pictureBoxDish;
+        private System.Windows.Forms.PictureBox pictureBoxFruit;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem стартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ 16/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap[] imagesFruits = new Bitmap[4];
+
+        Rectangle rDish, rFruit;
+
+        int fallingSpeed;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left) pictureBoxDish.Left -= 15;
+            if (e.KeyData == Keys.Right) pictureBoxDish.Left += 15;
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void стартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap bitmap = new Bitmap(@"Images\fruit1.png");
+            imagesFruits[0] = (Bitmap)bitmap.Clone();
+            bitmap = new Bitmap(@"Images\fruit2.png");
+            imagesFruits[1] = (Bitmap)bitmap.Clone();
+            bitmap = new Bitmap(@"Images\fruit3.png");
+            imagesFruits[2] = (Bitmap)bitmap.Clone();
+            bitmap = new Bitmap(@"Images\fruit4.png");
+            imagesFruits[3] = (Bitmap)bitmap.Clone();
+
+
+            pictureBoxDish.Parent = pictureBG;
+            pictureBoxFruit.Parent = pictureBG;
+
+            Random random = new Random();
+            pictureBoxFruit.Image = imagesFruits[random.Next(0,3)];
+            pictureBoxDish.BackColor = Color.Transparent;
+            pictureBoxFruit.BackColor = Color.Transparent;
+
+            fallingSpeed = 10;
+
+            pictureBoxFruit.Top = -40;
+            pictureBoxFruit.Left = random.Next(200, 600);
+
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            pictureBoxFruit.Top += fallingSpeed;
+
+            if(pictureBoxFruit.Top >=pictureBG.Height)
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Конец игры");
+                
+            }
+
+            rDish = pictureBoxFruit.DisplayRectangle;
+            rDish.Location = pictureBoxFruit.Location;
+
+            rFruit = pictureBoxDish.DisplayRectangle;
+            rFruit.Location = pictureBoxDish.Location;
+
+            if(rDish.IntersectsWith(rFruit))
+            {
+                timer1.Enabled = false;
+                MessageBox.Show("Вы выиграли!");
+            }
+        }
+    }
+}
